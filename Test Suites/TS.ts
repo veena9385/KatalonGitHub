fe7f30b5e86a028c59d0fc7616945b8b4246cef4
veena.hurukadli <veena.hurukadli@BDC7-L-5384TC7.dir.svc.accenture.com>
2019-08-19 19:08:21 +0530

    Added Test suites

---
+++ Test Suites/TS.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>01ee6a49-709c-4583-a9fb-0b56d80427e7</testSuiteGuid>
+</TestSuiteEntity>
